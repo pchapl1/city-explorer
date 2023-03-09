eb2dc2e94187d231ece027e0b5f6c4fb4fd3bf22
Phil Chaplin <pchapl1@gmail.com>
2023-03-08 18:06:52 -0800

    finsished lab 7

---
+++ src/components/Weather.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+
+class Weather extends React.Component {
+
+    render () {
+
+        
+
+        console.log(this.props.data)
+        return (
+            <>
+                { this.props.data 
+                    &&
+                    <>
+                        <h3>Weather forecast</h3>
+                        <ListGroup>
+                        <ListGroup.Item>{this.props.data[0].date}: {this.props.data[0].description}</ListGroup.Item>
+                        <ListGroup.Item>{this.props.data[1].date}: {this.props.data[1].description}</ListGroup.Item>
+                        <ListGroup.Item>{this.props.data[2].date}: {this.props.data[2].description}</ListGroup.Item>
+                        </ListGroup>
+                    
+                    </>
+                }
+                                {
+                    this.props.errorMessage &&
+                    <p>{this.props.errorMessage}</p>
+                }
+            </>
+
+            )
+
+    }
+}
+
+export default Weather;
